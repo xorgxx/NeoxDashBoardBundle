664b6532bf6ba3649f5ba46c0fceaf11f55e70b9
Xorg <xorg@neonatis.com>
2024-10-21 12:38:51 +0200

    Fix small bug and refactoring code.

--- assets/dist/neox_drag_nav_domain_controller.js
+++ assets/dist/neox_drag_nav_domain_controller.js
@@ -6,22 +6,43 @@ export default class extends Controller {
     connect(){
         // Liez les méthodes aux événements pour les écouter
         this.handleDragStart    = this.handleDragStart.bind(this);
-        
+        // this.handleDragOver     = this.handleDragOver.bind(this);
+        // this.handleDragLeave    = this.handleDragLeave.bind(this);
         // Attachez les écouteurs d'événements
         this.element.addEventListener('dragstart', this.handleDragStart);
+        // this.element.addEventListener('dragover', this.handleDragOver);
+        // this.element.addEventListener('dragleave', this.handleDragLeave);
     }
     
     disconnect(){
         // Supprimez les écouteurs d'événements lors de la déconnexion
         this.element.removeEventListener('dragstart', this.handleDragStart);
+        // this.element.removeEventListener('dragover', this.handleDragOver);
+        // this.element.removeEventListener('dragleave', this.handleDragLeave);
     }
     
     handleDragStart(event){
         // Vérifiez si l'élément draggable est la cible
         const item = event.target.closest('[data-xorgxx--neox-dashboard-bundle--neox-drag-domain-target="item"]');
-        if(item){
-            event.dataTransfer.setData("text/plain", item.dataset.site);
-            item.classList.add('dragging'); // Add visual effect during drag
+        event.dataTransfer.setData("text/plain", item.dataset.site);
+        item.classList.add('dragging'); // Add visual effect during drag
+ 
+    }
+    
+    handleDragOver(event){
+        event.preventDefault();
+        const targetElement = event.target.closest('[data-xorgxx--neox-dashboard-bundle--neox-drag-domain-target="item"]');
+        const domainChild = targetElement.querySelector('.domain'); // Find child with class 'domain'
+        if(domainChild){
+            domainChild.classList.add('drag-hover'); // Add hover effect during drag on the child
+        }
+    }
+    
+    handleDragLeave(event){
+        const targetElement = event.target.closest('[data-xorgxx--neox-dashboard-bundle--neox-drag-domain-target="item"]');
+        const domainChild = targetElement.querySelector('.domain'); // Find child with class 'domain'
+        if(domainChild){
+            domainChild.classList.remove('drag-hover'); // Remove hover effect during drag on the child
         }
     }
 }
