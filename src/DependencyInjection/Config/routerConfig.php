46b4ab267b820c0949bee9491f958609df492eb5
Xorg <xorg@neonatis.com>
2024-10-25 09:17:36 +0200

    Fix installation importmap.php setup config easier.

--- src/DependencyInjection/Config/routerConfig.php
+++ src/DependencyInjection/Config/routerConfig.php
@@ -12,14 +12,24 @@
         ]);
     */
 
+    #controllers_neox_dashboard:
+#    resource:
+#        path: "../vendor/xorgxx/neox-dashboard-bundle/src/Controller/"
+#        namespace: NeoxDashBoard\NeoxDashBoardBundle\Controller
+#    #        prefix: "/secure"
+#    #        trailing_slash_on_root: true
+#    type: attribute
+
     class routerConfig
     {
         public static function getConfig(): array
         {
             return [
                 'controllers_neox_dashboard' => [
-                    'resource'  => '../vendor/xorgxx/neox-dashboard-bundle/src/Controller/',
-                    'namespace' => 'NeoxDashBoard\NeoxDashBoardBundle\Controller',
+                    'resource'  => [
+                        'path'      => '../vendor/xorgxx/neox-dashboard-bundle/src/Controller/',
+                        'namespace' => 'NeoxDashBoard\NeoxDashBoardBundle\Controller',
+                    ],
                     'type'      => 'attribute',
                     // 'prefix' => '/secure', // Ajoutez un préfixe si nécessaire
                 ],
