b4c15ee93b159e0fa3b01c8a8766258d8be53e0f
Xorg <xorg@neonatis.com>
2024-11-07 09:44:23 +0100

    Fixing bug's and add css style more friendly

--- src/Form/NeoxDashSectionType.php
+++ src/Form/NeoxDashSectionType.php
@@ -6,7 +6,7 @@
     use Symfony\Component\Form\Extension\Core\Type\Integer;
     use Symfony\Component\Form\AbstractType;
     use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-    use Symfony\Component\Form\Extension\Core\Type\CheckBoxType;;
+    use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
     use Symfony\Component\Form\Extension\Core\Type\NumberType;
     use Symfony\Component\Form\FormBuilderInterface;
     use Symfony\Component\OptionsResolver\OptionsResolver;
